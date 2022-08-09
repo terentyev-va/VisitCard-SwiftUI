f99d54bc581765471b0643f0ad53e10421971835
terentyev-va <terentyev.va@yandex.ru>
2022-08-09 09:39:03 +0300

    Initial Commit

---
+++ VisitCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisitCard
+//
+//  Created by Вячеслав Терентьев on 09.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisitCard/VisitCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitCardApp.swift
+//  VisitCard
+//
+//  Created by Вячеслав Терентьев on 09.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct VisitCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
